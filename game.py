2d65c3092da4b61840982abbe0ba00cc0747522c
Homyak228 <gia.100811@gmail.com>
2024-11-01 12:24:52 +0300

    Добавлены звуки и кнопки с буквами

--- game.py
+++ game.py
@@ -1,26 +1,55 @@
 import pygame
 import settings
+from settings import LETTER_HEIGHT, LETTER_OFFSET, LETTER_START_POS
 
 
-def start():
+def start_game():
     pygame.init()
 
     # Настройка
     window = pygame.display.set_mode([settings.WIDTH, settings.HEIGHT])
     pygame.display.set_caption("Hangman")
     pygame.display.set_icon(pygame.image.load("./assets/sprites/icon.png"))
+    game_font = pygame.font.Font("./assets/fonts/HaginCapsMedium-Medium.ttf", 32)
 
     background_image = pygame.transform.scale(pygame.image.load("./assets/sprites/Wild_west.png"), [settings.WIDTH, settings.HEIGHT])
+    letter_button = pygame.transform.scale(pygame.image.load("./assets/sprites/button.png"), [settings.LETTER_WIDTH, settings.LETTER_HEIGHT])
+    button_image = pygame.transform.scale(pygame.image.load("./assets/sprites/button.png"), [90, 40])
+    no_image = pygame.transform.scale(pygame.image.load("./assets/sprites/no.png"), [30,35])
 
     run = True
     while run:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                mouse_x, mouse_y = pygame.mouse.get_pos()
+
+                if mouse_x >= 300 and mouse_x <= 390:
+                    if mouse_y >= 10 and mouse_y <= 50:
+                        run = False
 
         window.blit(background_image, [0,0])
+
+        # Отрисовка кнопок с буквами
+        for x in range(10):
+            window.blit(letter_button, [2 + x * (35 + 5), LETTER_START_POS])
+            window.blit(game_font.render(str(chr(1072 + x)), True, [255, 138, 0]), [12 + x * (35 + 5), LETTER_START_POS - 5])
+        for x in range(10):
+            window.blit(letter_button, [2 + x * (35 + 5), LETTER_START_POS + LETTER_HEIGHT + LETTER_OFFSET])
+            window.blit(game_font.render(str(chr(1082 + x)), True, [255, 138, 0]), [12 + x * (35 + 5), LETTER_START_POS - 5 + LETTER_HEIGHT + LETTER_OFFSET])
+        for x in range(10):
+            window.blit(letter_button, [2 + x * (35 + 5), LETTER_START_POS + (LETTER_HEIGHT + LETTER_OFFSET) * 2])
+            window.blit(game_font.render(str(chr(1092 + x)), True, [255, 138, 0]), [12 + x * (35 + 5), LETTER_START_POS - 5 + (LETTER_HEIGHT + LETTER_OFFSET) * 2])
+        for x in range(2):
+            window.blit(letter_button, [150 + x * (35 + 5), LETTER_START_POS + (LETTER_HEIGHT + LETTER_OFFSET) * 3])
+            window.blit(game_font.render(str(chr(1102 + x)), True, [255, 138, 0]), [160 + x * (35 + 5), LETTER_START_POS - 5 + (LETTER_HEIGHT + LETTER_OFFSET) * 3])
+
+        window.blit(button_image, [300, 10])
+        window.blit(game_font.render("Выход", True, [255, 65, 0]), [305, 10])
 
         pygame.display.update()
 
 if __name__ == "__main__":
-    start()
+    start_game()
+    pygame.quit()
--- menu.py
+++ menu.py
@@ -1,56 +1,66 @@
 import pygame
 import settings
-from game import start
+from game import start_game
 
-pygame.init()
+def start_menu():
+    pygame.init()
 
-# Настройка
-window = pygame.display.set_mode([settings.WIDTH,settings.HEIGHT])
-pygame.display.set_caption("Hangman")
-pygame.display.set_icon(pygame.image.load("./assets/sprites/icon.png"))
-menu_font = pygame.font.Font("./assets/fonts/HaginCapsMedium-Medium.ttf", 50)
+    # Настройка
+    window = pygame.display.set_mode([settings.WIDTH,settings.HEIGHT])
+    pygame.display.set_caption("Hangman")
+    pygame.display.set_icon(pygame.image.load("./assets/sprites/icon.png"))
+    menu_font = pygame.font.Font("./assets/fonts/HaginCapsMedium-Medium.ttf", 50)
 
-# Загрузка спрайтов
-background_image = pygame.transform.scale(pygame.image.load("./assets/sprites/Wild_west.png"), [settings.WIDTH, settings.HEIGHT])
-button_image = pygame.transform.scale(pygame.image.load("./assets/sprites/button.png"), [settings.BUTTON_WIDTH, settings.BUTTON_HEIGHT])
+    # Загрузка спрайтов
+    background_image = pygame.transform.scale(pygame.image.load("./assets/sprites/Wild_west.png"), [settings.WIDTH, settings.HEIGHT])
+    button_image = pygame.transform.scale(pygame.image.load("./assets/sprites/button.png"), [settings.BUTTON_WIDTH, settings.BUTTON_HEIGHT])
 
-run = True
-while run:
-    # Обработка событий
-    for event in pygame.event.get():
-        # Выход из приложения
-        if event.type == pygame.QUIT:
-            run = False
-        # Проверка нажатия на кнопку
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            # Получение координат курсора
-            mouse_x, mouse_y = pygame.mouse.get_pos()
+    # Загрузка музыки
+    pygame.mixer.music.load("./assets/sounds/Background_music.mp3")
+    pygame.mixer.music.set_volume(0)
+    pygame.mixer.music.play(1, 0, 0)
+    click = pygame.mixer.Sound("./assets/sounds/click.mp3")
 
-            # Проверка нажатия на кнопку играть
-            if mouse_x >= 105 and mouse_x <= 105 + settings.BUTTON_WIDTH:
-                if mouse_y >= 170 and mouse_y <= 230:
-                    start()
-                    run = False
+    run = True
+    while run:
+        # Обработка событий
+        for event in pygame.event.get():
+            # Выход из приложения
+            if event.type == pygame.QUIT:
+                run = False
+            # Проверка нажатия на кнопку
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                # Получение координат курсора
+                mouse_x, mouse_y = pygame.mouse.get_pos()
 
-            # Проверка нажатия на кнопку выйти
-            if mouse_x >= 105 and mouse_x <= 105 + settings.BUTTON_WIDTH:
-                if mouse_y >= 260 and mouse_y <= 320:
-                    run = False
+                # Проверка нажатия на кнопку играть
+                if mouse_x >= 105 and mouse_x <= 105 + settings.BUTTON_WIDTH:
+                    if mouse_y >= 170 and mouse_y <= 230:
+                        click.play()
+                        start_game()
+                        run = False
 
-    # Отрисовка фона
-    window.blit(background_image, [0,0])
+                # Проверка нажатия на кнопку выйти
+                if mouse_x >= 105 and mouse_x <= 105 + settings.BUTTON_WIDTH:
+                    if mouse_y >= 260 and mouse_y <= 320:
+                        run = False
 
-    # Отрисовка кнопки играть
-    window.blit(button_image, [105,170])
-    window.blit(menu_font.render("Играть", True, [255, 138, 0]), [130,168])
-    # Отрисовка кнопки выйти
-    window.blit(button_image, [105, 260])
-    window.blit(menu_font.render("Выйти", True, [255, 65, 0]), [130, 258])
+        # Отрисовка фона
+        window.blit(background_image, [0,0])
 
-    # Отрисовка логотипа
-    window.blit(menu_font.render("Виселица", True, [255, 210, 0]), [98, 25])
+        # Отрисовка кнопки играть
+        window.blit(button_image, [105,170])
+        window.blit(menu_font.render("Играть", True, [255, 138, 0]), [130,168])
+        # Отрисовка кнопки выйти
+        window.blit(button_image, [105, 260])
+        window.blit(menu_font.render("Выйти", True, [255, 65, 0]), [130, 258])
 
-    # Обновление экрана
-    pygame.display.update()
+        # Отрисовка логотипа
+        window.blit(menu_font.render("Виселица", True, [255, 210, 0]), [98, 25])
 
-pygame.quit()
+        # Обновление экрана
+        pygame.display.update()
+
+if __name__ == "__main__":
+    start_menu()
+    pygame.quit()
